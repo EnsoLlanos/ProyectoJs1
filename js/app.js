b9339c7eb13624a386bba540f51b955e69270d62
Enso LLanos <ensollanos@gmail.com>
2023-12-18 20:54:55 -0500

    Avanzando con el carrito

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+    
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
+    }
+}
+function leerDatosCurso(curso) {
+    console.log(curso);
+
+    // Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+}
